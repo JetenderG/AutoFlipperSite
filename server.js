29c8cc60ac7efb22477f9f73d8a1c9dca6b48c82
Jetender <g.jetender@gmail>
2019-06-19 01:45:25 -0400

    Sefsf

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path")
 const db = require("./models");
 const session = require("express-session");
 const app = express();
-const PORT = process.env.JAWSDB_URL || 3000;
+const PORT = process.env.JAWSDB_URL || 3306;
 
 
 app.use(session({
