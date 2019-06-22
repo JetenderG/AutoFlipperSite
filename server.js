e3ba8ccb17b4811c92791e6370f2a38d78156581
Jetender <g.jetender@gmail>
2019-06-22 02:48:58 -0400

    heroku server needs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path")
 const db = require("./models");
 const session = require("express-session");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.JAWSDB_URL || 3000;
 
 //
 app.use(session({
