4539be4a13c85368386673c9b672fa313efe31e8
Jetender <g.jetender@gmail>
2019-06-22 02:31:29 -0400

    server issues

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path")
 const db = require("./models");
 const session = require("express-session");
 const app = express();
-const PORT = process.env.JAWSDB_URL || 3000;
+const PORT = process.env.PORT || 3000;
 
 //
 app.use(session({
@@ -27,7 +27,7 @@ app.engine(
   exphbs({
     defaultLayout: "main",
   })
-);//
+); //
 app.set("view engine", "handlebars");
 
 // Routes
